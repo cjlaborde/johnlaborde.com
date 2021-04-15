c9b5b8d3839e67a1a287989b023c135241630067
cjlaborde <cjlaborde@gmail.com>
2021-04-15 07:33:41 -0400

    Removed nuxt composition to see it works

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,60 +1,43 @@
 <template>
   <div v-if="articles">
-    <div v-if="$fetchState.pending">Loading Search</div>
+    <div>
+      <h3 class="font-semibold text-gray-600 text-lg mb-10">
+        Search results: ({{ articles.length }})
+      </h3>
+    </div>
+
+    <div v-if="articles.length > 0">
+      <ArticleList :articles="articles" />
+    </div>
     <div v-else>
-      <div>
-        <h3 class="font-semibold text-gray-600 text-lg mb-10">
-          Search results: ({{ articles.length }})
-        </h3>
-      </div>
-
-      <div v-if="articles.length > 0">
-        <ArticleList :articles="articles" />
-      </div>
-
-      <div v-else>
-        <h3 class="text-gray-500 text-sm">No Search result</h3>
-      </div>
+      <h3 class="text-gray-500 text-sm">No Search result</h3>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import {
-  defineComponent,
-  useContext,
-  useFetch,
-  useRoute,
-  computed,
-  watch,
-  ref,
-} from '@nuxtjs/composition-api';
-
-export default defineComponent({
-  name: 'SearchPage',
-  setup() {
-    const { $content } = useContext();
-    const articles = ref<any>([]);
-    const route = useRoute();
-
-    const searchQuery = computed(() => route.value.query.keyword);
-
-    const { fetchState, fetch } = useFetch(async () => {
-      articles.value = [];
-      if (searchQuery.value) {
-        articles.value = await $content('articles')
-          .only(['title', 'description', 'image', 'slug', 'published', 'tags'])
-          .sortBy('published', 'desc')
-          .search(searchQuery.value)
-          .fetch();
-      }
-    });
-
-    watch(searchQuery, () => {
-      fetch();
-    });
-
-    return { articles, fetchState };
+<script>
+export default {
+  data() {
+    return {
+      articles: [],
+    };
   },
-});
+  computed: {
+    searchQuery() {
+      return this.$route.query.keyword;
+    },
+  },
+  watch: {
+    async searchQuery(searchQuery) {
+      if (!searchQuery) {
+        this.articles = [];
+        return;
+      }
+      this.articles = await this.$content('articles')
+        .limit(6)
+        .search(searchQuery)
+        .fetch();
+    },
+  },
+};
 </script>
