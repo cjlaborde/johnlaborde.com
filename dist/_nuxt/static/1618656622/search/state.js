ef88eb659d2e74613a818c3764e92253e92aeda7
cjlaborde <cjlaborde@gmail.com>
2021-04-17 06:52:37 -0400

    used generate

---
+++ dist/_nuxt/static/1618656622/search/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618656622",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsearch",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2c120235"}},globalRefs:{}}}(null));
