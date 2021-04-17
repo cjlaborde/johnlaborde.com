4cc11c418d04da52d8a972cbc9d68dbc1ac5917b
cjlaborde <cjlaborde@gmail.com>
2021-04-17 10:40:49 -0400

    used generate

---
+++ dist/_nuxt/static/1618670263/search/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618670263",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsearch",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8a27c3fd"}},globalRefs:{}}}(null));
