b7b7912f9bec9fe4da3aed8915650331ca786610
cjlaborde <cjlaborde@gmail.com>
2021-04-17 21:51:06 -0400

    used generate

---
+++ dist/_nuxt/static/1618710572/tags/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618710572",layout:"default",error:a,serverRendered:false,routePath:"\u002Ftags",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5cd7c838"}},globalRefs:{}}}(null));
